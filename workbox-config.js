141c22a6837b2df1198cc3d9224b392f6c60796c
Andrew Simon <46216553+andrew1simon@users.noreply.github.com>
2023-08-09 17:50:09 +0300

    added runtime cache

--- workbox-config.js
+++ workbox-config.js
@@ -1,5 +1,20 @@
 module.exports = {
-	
+	runtimeCaching: [
+		{
+		  urlPattern: /^https:\/\/cdnjs\.cloudflare\.com\/ajax\/libs\/font\-awesome\/.*/i,
+		  handler: 'CacheFirst',
+		  options: {
+			cacheName: 'fontaw-cache',
+			expiration: {
+			  maxEntries: 10,
+			  maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+			},
+			cacheableResponse: {
+			  statuses: [0, 200]
+			}
+		  }
+		}
+	  ],
 	globDirectory: './',
 	globPatterns: [
 		'**/*.{html,png,jpg,jpeg,css,json,ico}'
