dd5c0924f034342f9bfa992e8810bb27f497aab6
Andrew Simon <46216553+andrew1simon@users.noreply.github.com>
2023-08-09 18:08:41 +0300

    added google font cache

--- workbox-config.js
+++ workbox-config.js
@@ -14,6 +14,22 @@ module.exports = {
 			}
 		  }
 		}
+		,
+		{
+			// https://fonts.googleapis.com/css2?family=Cairo:wght@400;500;600;700&family=Josefin+Sans:wght@100&family=Oswald:wght@300;500;600&family=Roboto+Condensed&family=Ysabeau+Infant:ital,wght@0,300;1,200&family=Ysabeau+SC:wght@100;200;300;400&display=swap
+			urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+			handler: 'CacheFirst',
+			options: {
+			  cacheName: 'fontaw-cache',
+			  expiration: {
+				maxEntries: 10,
+				maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+			  },
+			  cacheableResponse: {
+				statuses: [0, 200]
+			  }
+			}
+		  }
 	  ],
 	globDirectory: './',
 	globPatterns: [
